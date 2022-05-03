63f7eb1f58cb19ebcdec0accf41ce0ea19b155d4
Mike Kienenberger <mkienenb@gmail.com>
2022-05-03 18:41:20 -0400

    test: each appointment list element is a button

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -71,4 +71,10 @@ describe('AppointmentsDayView', () => {
         render(<AppointmentsDayView appointments={appointments}/>);
         expect(container.textContent).toMatch('Ashley');
     });
+
+    it('has a button element in each li element', () => {
+        render(<AppointmentsDayView appointments={appointments}/>);
+        expect(container.querySelectorAll('li > button')).toHaveLength(2);
+        expect(container.querySelectorAll('li > button')[0].type).toEqual('button');
+    });
 });
