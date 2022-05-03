d432c5d83b659ce52b612bbb97cecaac57bb4f39
Mike Kienenberger <mkienenb@gmail.com>
2022-05-03 18:15:58 -0400

    test: show no appointments message initially

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -55,4 +55,9 @@ describe('AppointmentsDayView', () => {
         expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
         expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00');
     });
+
+    it('Initially shows a message that there are no appointments today', () => {
+        render(<AppointmentsDayView appointments={[]}/>);
+        expect(container.textContent).toMatch('There are no appointments scheduled for today.');
+    });
 });
