11f30413f49654833142c43fcde5601b6d3cf870
Mike Kienenberger <mkienenb@gmail.com>
2022-05-03 18:54:50 -0400

    refactor: wrap const def in braces and return existing value (bug in book, p53, missing p49-50 changes)

--- src/Appointment.js
+++ src/Appointment.js
@@ -8,19 +8,23 @@ const appointmentTimeOfDay = startsAt => {
 
 export const Appointment = ({customer}) => <div>{customer.firstName}</div>;
 
-export const AppointmentsDayView = ({appointments}) => <div id="appointmentsDayView">
-    <ol>
-        {appointments.map(appointment => (
-            <li key={appointment.startsAt}>
-                <button type="button">
-                    {appointmentTimeOfDay(appointment.startsAt)}
-                </button>
-            </li>
-        ))}
-    </ol>
-    {appointments.length === 0 ? (
-        <p>There are no appointments scheduled for today.</p>
-    ) : (
-        <Appointment {...appointments[0]} />
-    )}
-</div>;
+export const AppointmentsDayView = ({appointments}) => {
+    return (
+        <div id="appointmentsDayView">
+            <ol>
+                {appointments.map(appointment => (
+                    <li key={appointment.startsAt}>
+                        <button type="button">
+                            {appointmentTimeOfDay(appointment.startsAt)}
+                        </button>
+                    </li>
+                ))}
+            </ol>
+            {appointments.length === 0 ? (
+                <p>There are no appointments scheduled for today.</p>
+            ) : (
+                <Appointment {...appointments[0]} />
+            )}
+        </div>
+    );
+};
