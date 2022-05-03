993cca653fd61217775aebfcf7be8891cae39cbe
Mike Kienenberger <mkienenb@gmail.com>
2022-05-03 17:58:05 -0400

    test: change appointment element container from div to li.

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -48,4 +48,16 @@ describe('AppointmentsDayView', () => {
         expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
         expect(container.querySelector('ol').children).toHaveLength(2);
     });
+
+    it('renders each appointment in an li', () => {
+        const today = new Date();
+        const appointments = [
+            {startsAt: today.setHours(12, 0)},
+            {startsAt: today.setHours(13, 0)}
+        ];
+        render(<AppointmentsDayView appointments={appointments}/>);
+        expect(container.querySelectorAll('li')).toHaveLength(2);
+        expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
+        expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00');
+    });
 });
