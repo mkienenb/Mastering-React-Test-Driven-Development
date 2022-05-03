3db1bee8e8e672135a2915e75dcf93e39e8b411f
Mike Kienenberger <mkienenb@gmail.com>
2022-05-03 18:11:19 -0400

    test: extract common code.

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -25,6 +25,11 @@ describe('Appointment', () => {
 });
 
 describe('AppointmentsDayView', () => {
+    const today = new Date();
+    const appointments = [
+        {startsAt: today.setHours(12, 0)},
+        {startsAt: today.setHours(13, 0)}
+    ];
     let container;
 
     beforeEach(() => {
@@ -39,22 +44,12 @@ describe('AppointmentsDayView', () => {
     });
 
     it('renders multiple appointments in an ol element', () => {
-        const today = new Date();
-        const appointments = [
-            {startsAt: today.setHours(12, 0)},
-            {startsAt: today.setHours(13, 0)}
-        ];
         render(<AppointmentsDayView appointments={appointments}/>);
         expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
         expect(container.querySelector('ol').children).toHaveLength(2);
     });
 
     it('renders each appointment in an li', () => {
-        const today = new Date();
-        const appointments = [
-            {startsAt: today.setHours(12, 0)},
-            {startsAt: today.setHours(13, 0)}
-        ];
         render(<AppointmentsDayView appointments={appointments}/>);
         expect(container.querySelectorAll('li')).toHaveLength(2);
         expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
