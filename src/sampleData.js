6594bf00403083d5b5c4a2d190232e68003eec15
Mike Kienenberger <mkienenb@gmail.com>
2022-05-04 14:58:18 -0400

    chore: use existing sample data for manual testing

--- src/sampleData.js
+++ src/sampleData.js
@@ -3,13 +3,30 @@ const today = new Date();
 const at = hours => today.setHours(hours, 0);
 
 export const sampleAppointments = [
-  { startsAt: at(9), customer: { firstName: 'Charlie' } },
-  { startsAt: at(10), customer: { firstName: 'Frankie' } },
-  { startsAt: at(11), customer: { firstName: 'Casey' } },
-  { startsAt: at(12), customer: { firstName: 'Ashley' } },
-  { startsAt: at(13), customer: { firstName: 'Jordan' } },
-  { startsAt: at(14), customer: { firstName: 'Jay' } },
-  { startsAt: at(15), customer: { firstName: 'Alex' } },
-  { startsAt: at(16), customer: { firstName: 'Jules' } },
-  { startsAt: at(17), customer: { firstName: 'Stevie' } }
+
+    {
+        startsAt: today.setHours(12, 0),
+        customer: {firstName: 'Ashley', lastName: 'Benton', phoneNumber: '123-5678'},
+        stylist: 'Judy',
+        service: 'Coloring',
+        notes: 'Likes Pink'
+    },
+    {
+        startsAt: today.setHours(13, 0),
+        customer: {firstName: 'Jordan', lastName: 'Kline', phoneNumber: '321-9876'},
+        stylist: 'Joe',
+        service: 'Beard Trim'
+    }
+];
+
+export const sampleAppointmentsOld = [
+    {startsAt: at(9), customer: {firstName: 'Charlie'}},
+    {startsAt: at(10), customer: {firstName: 'Frankie'}},
+    {startsAt: at(11), customer: {firstName: 'Casey'}},
+    {startsAt: at(12), customer: {firstName: 'Ashley'}},
+    {startsAt: at(13), customer: {firstName: 'Jordan'}},
+    {startsAt: at(14), customer: {firstName: 'Jay'}},
+    {startsAt: at(15), customer: {firstName: 'Alex'}},
+    {startsAt: at(16), customer: {firstName: 'Jules'}},
+    {startsAt: at(17), customer: {firstName: 'Stevie'}}
 ];
