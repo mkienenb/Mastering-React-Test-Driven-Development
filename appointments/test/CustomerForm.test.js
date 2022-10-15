c7688ae402b10cf3012c5a9d42a7b71aa44e826c
Mike Kienenberger <mkienenb@gmail.com>
2022-10-15 18:47:26 -0400

    test: generalize firstNameField() method

--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -12,7 +12,7 @@ describe('CustomerForm', () => {
 
     const form = id => container.querySelector(`form[id="${id}"]`);
     const labelFor = formElement => container.querySelector(`label[for="${formElement}"]`);
-    const firstNameField = () => form('customer').elements.firstName;
+    const field = name => form('customer').elements[name];
 
     const expectToBeInputFieldOfTypeText = formElement => {
         expect(formElement).not.toBeNull();
@@ -28,12 +28,12 @@ describe('CustomerForm', () => {
     describe('first name field', () => {
         it('renders the first name field as a text box', () => {
             render(<CustomerForm />);
-            expectToBeInputFieldOfTypeText(firstNameField());
+            expectToBeInputFieldOfTypeText(field('firstName'));
         });
 
         it('includes the existing value for the first name', () => {
             render(<CustomerForm firstName="Ashley" />);
-            expect(firstNameField().value).toEqual('Ashley');
+            expect(field('firstName').value).toEqual('Ashley');
         });
 
         it('renders a label for the first name field', () => {
@@ -44,7 +44,7 @@ describe('CustomerForm', () => {
 
         it('assigns an id that matches the label id to the first name field', () => {
             render(<CustomerForm />);
-            expect(firstNameField().id).toEqual('firstName');
+            expect(field('firstName').id).toEqual('firstName');
         });
 
         it('saves existing first name when submitted', async () => {
@@ -70,7 +70,7 @@ describe('CustomerForm', () => {
                     }
                 />
             );
-            await ReactTestUtils.Simulate.change(firstNameField(), {
+            await ReactTestUtils.Simulate.change(field('firstName'), {
                 target: { value: "Jamie" }
             });
             await ReactTestUtils.Simulate.submit(form('customer'));
