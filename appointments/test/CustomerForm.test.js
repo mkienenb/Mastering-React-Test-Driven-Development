616c6d23ab6284465e2c6b9dcfb6010a7b9fd591
Mike Kienenberger <mkienenb@gmail.com>
2022-10-15 18:56:32 -0400

    test: move itRendersAsATextBox and expectToBeInputFieldOfTypeText functions up one level

--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -4,34 +4,34 @@ import { createContainer } from "./domManipulators";
 import { CustomerForm } from '../src/CustomerForm';
 
 describe('CustomerForm', () => {
-    let render, container;
-
-    beforeEach(() => {
-        ({ render, container } = createContainer());
-    });
-
-    const form = id => container.querySelector(`form[id="${id}"]`);
-    const labelFor = formElement => container.querySelector(`label[for="${formElement}"]`);
-    const field = name => form('customer').elements[name];
-
     const expectToBeInputFieldOfTypeText = formElement => {
         expect(formElement).not.toBeNull();
         expect(formElement.tagName).toEqual('INPUT');
         expect(formElement.type).toEqual('text');
     }
 
+    const itRendersAsATextBox = (fieldName) => {
+        it('renders the first name field as a text box', () => {
+            render(<CustomerForm/>);
+            expectToBeInputFieldOfTypeText(field(fieldName));
+        });
+    }
+
+    let render, container;
+    beforeEach(() => {
+        ({ render, container } = createContainer());
+    });
+    const form = id => container.querySelector(`form[id="${id}"]`);
+
+    const labelFor = formElement => container.querySelector(`label[for="${formElement}"]`);
+
+    const field = name => form('customer').elements[name];
+
     it('renders a form', ()=> {
         render(<CustomerForm />)
         expect(form('customer')).not.toBeNull();
     });
-
     describe('first name field', () => {
-        const itRendersAsATextBox = (fieldName) => {
-            it('renders the first name field as a text box', () => {
-                render(<CustomerForm/>);
-                expectToBeInputFieldOfTypeText(field(fieldName));
-            });
-        }
         itRendersAsATextBox('firstName');
 
         it('includes the existing value for the first name', () => {
