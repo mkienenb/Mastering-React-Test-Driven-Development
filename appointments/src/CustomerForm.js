9370b07fbd0a81818663e281485a1d95f5a14e24
Mike Kienenberger <mkienenb@gmail.com>
2022-10-15 19:41:35 -0400

    feature: add lastName inputs

--- appointments/src/CustomerForm.js
+++ appointments/src/CustomerForm.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 
-export const CustomerForm = ({firstName, onSubmit}) =>  {
-    const [ customer, setCustomer ] = useState({ firstName });
+export const CustomerForm = ({firstName, lastName, onSubmit}) =>  {
+    const [ customer, setCustomer ] = useState({ firstName, lastName });
     const handleChangeFirstName = ({ target }) =>
         setCustomer(customer => ({
             ...customer,
             firstName: target.value
+        }));
+    const handleChangeLastName = ({ target }) =>
+        setCustomer(customer => ({
+            ...customer,
+            lastName: target.value
         }));
     return <form id="customer" onSubmit={() => onSubmit(customer)}>
         <label htmlFor="firstName">First name</label>
@@ -14,6 +19,13 @@ export const CustomerForm = ({firstName, onSubmit}) =>  {
                name="firstName"
                value={firstName}
                onChange={handleChangeFirstName}
+        />
+        <label htmlFor="lastName">Last name</label>
+        <input id="lastName"
+               type="text"
+               name="lastName"
+               value={lastName}
+               onChange={handleChangeLastName}
         />
     </form>;
 };
